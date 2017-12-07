aca576bdc3d6af2985c5d40b53553d12d23bd1c9
Vince <blomvin@gmail.com>
2017-12-07 17:52:28 -0600

    more werk

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { forEach } from '../../Library/Caches/typescript/2.6/node_modules/@types/async';
+
 const Discord = require('discord.js');
 const cron = require('cron').CronJob
 const reminders = require('./functions/reminders')
@@ -12,25 +14,49 @@ if (!isProd) {
 }
 
 
+//Ready message
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
+//Messaging functionality
 client.on('message', message => {
+  //First check to see if the message is meant to be a command
+  if (message.content.split("")[0] === '$') { 
+
     if (message.content === 'ping') {
         message.reply('pong');
     }
+
+    //If message is meant to SET a reminder
+    //Assuming reminder is '$reminder 12/8/17 11:00 We have a meeting'
+    if (message.content.split(" ")[0] === '$reminder') {
+      //Get the title, date and channel from the message
+      var msg = message.content.split(" ")
+      var channel = message.channel.id
+      var date = msg[1] + msg[2]
+      var title = ''
+      for (i = 3; i < msg.length; i++) {
+        title += msg[i]
+      }
+      reminders.create(title, date, channel)
+    }
+  }
 });
 
 client.login(process.env.DISCORD_TOKEN);
 
-new cron('00 * * * * *', function() {
+//CRON SHIT
+new cron('00 * * * * *', () => {
   const date = new Date()
 
   const data = reminders.checkTime(date)
 
   if (data) {
-    //Send the message to the proper channel
+    //Loop over the reminders that need to be sent
+    data.forEach(reminder => {
+      //Send the message to the proper channel
+    })
   } else {
     //handle it
   }
